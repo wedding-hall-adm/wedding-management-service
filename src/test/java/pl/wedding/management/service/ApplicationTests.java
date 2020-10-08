674ebbcf9d25fd6131ee079c2f56cfc38582200e
msienkiewicz3 <msienkiewicz3@sii.pl>
2020-10-08 20:53:20 +0200

    Project created

---
+++ src/test/java/pl/wedding/management/service/ApplicationTests.java
@@ -0,0 +1,14 @@
+package pl.wedding.management.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
