d05b53c341f23692ef858af9545b43328681c697
BarbarBarabasz <barbarski@interia.eu>
2020-10-15 22:59:26 +0200

    Create wedding hall controller

---
+++ src/main/java/pl/wedding/management/weddinghall/WeddingHall.java
@@ -0,0 +1,28 @@
+package pl.wedding.management.weddinghall;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+public class WeddingHallController {
+
+    @Autowired
+    private WeddingHallService weddingHallService;
+
+    @RequestMapping("/weddinghalls")
+    public List<WeddingHall> getAllWeddingHalls(){
+        return weddingHallService.getAllWeddingHalls();
+    }
+
+    @RequestMapping("/weddinghalls/{id}")
+    public List(WeddingHall) getWeddingHall(@PathVariable
+    Long id){
+        return weddingHallService.getAllWeddingHalls(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/weddinghalls")
+    public void addWeddingHall(@RequestBody WeddingHall weddingHall){
+        weddingHallService.addWeddingHall(weddingHall);
+    }
+}
