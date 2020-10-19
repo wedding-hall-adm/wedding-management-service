1ef3c4c2cddd6c9cb621471a494e9390c4f6be1b
BartlomiejBak <bakbartlomiej88>
2020-10-19 13:59:29 +0200

    fix: delete getDateTime from ReservationRowFactory

--- src/main/java/pl/wedding/management/service/reservation/domain/converter/ReservationRowFactory.java
+++ src/main/java/pl/wedding/management/service/reservation/domain/converter/ReservationRowFactory.java
@@ -6,27 +6,17 @@ import pl.wedding.management.service.converter.RowFactory;
 import pl.wedding.management.service.reservation.domain.entity.ReservationEntity;
 import pl.wedding.management.service.reservation.domain.entity.ReservationDto;
 
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
-import java.util.Optional;
-
 @NoArgsConstructor
 public class ReservationRowFactory implements RowFactory<ReservationDto, ReservationEntity> {
 
     @Override
     public ReservationEntity convertFromDto(@NonNull ReservationDto dto) {
         return new ReservationEntity(dto.getId(),
-                getDateTime(dto.getReservationStart()),
-                getDateTime(dto.getReservationEnd()),
+                dto.getReservationStart(),
+                dto.getReservationEnd(),
                 dto.getWeddingHall(),
                 dto.getTenant(),
                 dto.getNumberOfGuests(),
                 dto.getOccasion());
-    }
-
-    private LocalDateTime getDateTime(LocalDateTime dateTime) {
-        return Optional.ofNullable(dateTime)
-                .map(input -> input.truncatedTo(ChronoUnit.MINUTES))
-                .orElse(null);
     }
 }
