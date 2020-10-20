453b0923fd3612d217c5f344d80e3db29184e418
lukas <lukasz.grazda@gmail.com>
2020-10-20 16:12:19 +0200

    WS-3_delete_@Value_add_Constructor

--- src/main/java/pl/wedding/management/service/weddinghall/model/AddressHall.java
+++ src/main/java/pl/wedding/management/service/weddinghall/model/AddressHall.java
@@ -3,11 +3,19 @@ package pl.wedding.management.service.weddinghall.model;
 import lombok.*;
 
 @Builder
-@Value
+@Getter
 public class AddressHall {
-    String country;
-    String city;
-    String street;
-    int numberBuilding;
-    String postCode;
+    private final String country;
+    private final String city;
+    private final String street;
+    private final int numberBuilding;
+    private final String postCode;
+
+    public AddressHall(String country, String city, String street, int numberBuilding, String postCode) {
+        this.country = country;
+        this.city = city;
+        this.street = street;
+        this.numberBuilding = numberBuilding;
+        this.postCode = postCode;
+    }
 }
--- src/main/java/pl/wedding/management/service/weddinghall/model/WeddingHall.java
+++ src/main/java/pl/wedding/management/service/weddinghall/model/WeddingHall.java
@@ -2,15 +2,23 @@ package pl.wedding.management.service.weddinghall.model;
 
 import lombok.*;
 
-import java.util.EnumSet;
 import java.util.Set;
 
-@Value
 @Builder
-
+@Getter
+@AllArgsConstructor
 public class WeddingHall {
-    Set<TypesOfHalls> typesOfHalls;
-    Set<HallsEquipment> hallEquipment;
-    String owner;
-    int max_guests;
+    private final Set<TypesOfHalls> typesOfHalls;
+    private final Set<HallsEquipment> hallEquipment;
+    private final String owner;
+    private final int max_guests;
+    private final int numberBuilding;
+
+    public WeddingHall(Set<TypesOfHalls> typesOfHalls, Set<HallsEquipment> hallEquipment, String owner, int max_guests, int numberBuilding) {
+        this.typesOfHalls = typesOfHalls;
+        this.hallEquipment = hallEquipment;
+        this.owner = owner;
+        this.max_guests = max_guests;
+        this.numberBuilding = numberBuilding;
+    }
 }
